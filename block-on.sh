91fe5be22f7a358806a7c40bab1f891d628db828
DUONG DANH VU <86816491+Dilysm@users.noreply.github.com>
2023-07-22 19:13:51 +0700

    Update block-on.sh

--- block-on.sh
+++ block-on.sh
@@ -1,6 +1,6 @@
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/5818c02087a0422081340424468798b2" \
-     -H "X-Auth-Email: aulax432@gmail.com" \
-     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+     -H "X-Auth-Email: dtai45412@gmail.com" \
+     -H "X-Auth-Key: 7f63bf3bcaa7a6759b9b2160cddba6723495f" \
      -H "Content-Type: application/json" \
      --data '{
      "action": "block",
@@ -9,7 +9,7 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12
      "description": "Block quốc tế",
      "filter": {
     "id": "cff5e0f781c14510b9d52e2fbdeb5123",
-    "expression": "(not ip.geoip.country in {\"VN\"} and http.user_agent contains \"iPhone\" and http.request.version in {\"HTTP/2\"                    \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"daukute.us\") or (not ip.geoip.country in {\"VN\"} and http.                   user_agent contains \"Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and htt                   p.host eq \"daukute.us\") or (not ip.geoip.country in {\"VN\"} and http.user_agent contains \"Windows\" and http.request.version in {\"HT                   TP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"daukute.us\")",
+    "expression": "(not ip.geoip.country in {\"JP\"} and http.user_agent contains \"iPhone\" and http.request.version in {\"HTTP/2\"                    \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"4gsoftbank.tech\") or (not ip.geoip.country in {\"JP\"} and http.                   user_agent contains \"Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and htt                   p.host eq \"4gsoftbank.tech\") or (not ip.geoip.country in {\"JP\"} and http.user_agent contains \"Windows\" and http.request.version in {\"HT                   TP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"4gsoftbank.tech\")",
     "paused": false,
     "description": "Block quốc tế"
   }
