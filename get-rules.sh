14d2ae63eb01ef28019a6ad97e0cf65271fdb205
DUONG DANH VU <86816491+Dilysm@users.noreply.github.com>
2023-07-22 19:14:07 +0700

    Update get-rules.sh

--- get-rules.sh
+++ get-rules.sh
@@ -1,4 +1,4 @@
 curl -X GET \
 "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules" \
--H "X-Auth-Email: aulax432@gmail.com" \
--H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"
+-H "X-Auth-Email: dtai45412@gmail.com" \
+-H "X-Auth-Key: 7f63bf3bcaa7a6759b9b2160cddba6723495f"
