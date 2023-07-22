6127592e59a37089d74fe893fa72776df6732620
DUONG DANH VU <86816491+Dilysm@users.noreply.github.com>
2023-07-22 19:41:26 +0700

    Update block-off.sh

--- block-off.sh
+++ block-off.sh
@@ -1,4 +1,4 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/5818c02087a0422081340424468798b2" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/4eeded2f5deb4538834dce862e877cc9" \
      -H "X-Auth-Email: dtai45412@gmail.com" \
      -H "X-Auth-Key: 7f63bf3bcaa7a6759b9b2160cddba6723495f" \
      -H "Content-Type: application/json" \
@@ -8,7 +8,7 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12
      "paused": true,
      "description": "Block quốc tế",
      "filter": {
-    "id": "cff5e0f781c14510b9d52e2fbdeb5123",
+    "id": "c14f1173927e47ad80598a1ca10ef4c3",
     "expression": "(not ip.geoip.country in {\"JP\"} and http.user_agent contains \"iPhone\" and http.request.version in {\"HTTP/2\"                    \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"4gsoftbank.tech\") or (not ip.geoip.country in {\"JP\"} and http.                   user_agent contains \"Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and htt                   p.host eq \"4gsoftbank.tech\") or (not ip.geoip.country in {\"JP\"} and http.user_agent contains \"Windows\" and http.request.version in {\"HT                   TP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"} and http.host eq \"4gsoftbank.tech\")",
     "paused": false,
     "description": "Block quốc tế"
